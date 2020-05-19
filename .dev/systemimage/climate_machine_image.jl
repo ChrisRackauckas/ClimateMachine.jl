ccb64d6add3fda60421b57c9fb7804c9db12c58d
jakebolewski <jakebolewski@gmail.com>
2020-05-19 16:20:23 -0600

    pre-build the project system image to use for testing

--- .dev/systemimage/climate_machine_image.jl
+++ .dev/systemimage/climate_machine_image.jl
@@ -11,28 +11,24 @@ sysimage_path =
 
 using Pkg
 Pkg.add("PackageCompiler")
+
 using PackageCompiler
 Pkg.activate(joinpath(@__DIR__, "..", ".."))
 Pkg.instantiate()
 pkgs = Pkg.installed()
 delete!(pkgs, "Pkg")
 
-# "TF_BUILD" is check to see if we are running on Azure for CI
-if !haskey(ENV, "TF_BUILD")
-    PackageCompiler.create_sysimage(
-        [Symbol(s) for s in keys(pkgs)];
-        sysimage_path = sysimage_path,
-        precompile_execution_file = joinpath(
-            @__DIR__,
-            "..",
-            "..",
-            "test",
-            "Numerics",
-            "DGmethods",
-            "Euler",
-            "isentropicvortex.jl",
-        ),
-    )
-else
-    return true
-end
+PackageCompiler.create_sysimage(
+    [Symbol(s) for s in keys(pkgs)];
+    sysimage_path = sysimage_path,
+    precompile_execution_file = joinpath(
+        @__DIR__,
+        "..",
+        "..",
+        "test",
+        "Numerics",
+        "DGmethods",
+        "Euler",
+        "isentropicvortex.jl",
+    ),
+)
